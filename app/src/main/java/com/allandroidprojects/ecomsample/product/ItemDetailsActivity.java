47073593f7faeed6848406a6970033cdaad4813e
Sayali Mirajkar <sayalimirajkar91@gmail.com>
2018-08-18 11:15:37 -0400

    Remove extra wishlist, share and similar text views

--- app/src/main/java/com/allandroidprojects/ecomsample/product/ItemDetailsActivity.java
+++ app/src/main/java/com/allandroidprojects/ecomsample/product/ItemDetailsActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.allandroidprojects.ecomsample.R;
